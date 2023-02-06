e19e588161b23f05e8e8e119ee2dab1efcf290d1
Linmiao Xu <linmiao.xu@gmail.com>
2023-02-06 05:37:55 +0000

    Add command to start net server

--- server/run_prod_services.sh
+++ server/run_prod_services.sh
@@ -2,6 +2,7 @@
 
 mongod --config /etc/mongod.conf &
 /app/fishtest/server/env/bin/pserve production.ini http_port=6543 &
+(cd /app/net-server && gunicorn net_server:app --workers 4 --worker-class uvicorn.workers.UvicornWorker) &
 nginx
 
 wait -n
