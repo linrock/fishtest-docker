e96fcd8cea662c58f62aa2cd1a164dafcbfafc87
Linmiao Xu <linmiao.xu@gmail.com>
2023-02-06 15:04:35 -0500

    Don't max out all the cores

--- worker/run_workers.sh
+++ worker/run_workers.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-num_workers=$(( $(nproc) / 16 ))
-for worker in $(seq 1 $num_workers); do
-  git clone https://github.com/glinscott/fishtest /root/worker$worker
-  cd /root/worker$worker/worker
-  python3 worker.py $WORKER_CONFIG &
+git clone https://github.com/glinscott/fishtest /root/fishtest
+
+num_16core_workers=$(( ($(nproc) - 1 ) / 16 ))
+for worker in $(seq 1 $num_16core_workers); do
+  worker_dir=/root/16core-worker$worker
+  cp -r fishtest $worker_dir
+  cd $worker_dir/worker
+  python3 worker.py $WORKER_CONFIG --concurrency 16 &
   cd /root
 done
 
+num_8core_workers=$(( ( $(nproc) - 1 - $num_16core_workers ) / 8 ))
+for worker in $(seq 1 $num_8core_workers); do
+  worker_dir=/root/8core-worker$worker
+  cp -r fishtest $worker_dir
+  cd $worker_dir/worker
+  python3 worker.py $WORKER_CONFIG --concurrency 8 &
+  cd /root
+done
 wait
