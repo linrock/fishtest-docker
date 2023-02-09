5e90289ee9dc698cf9dbb26decde6d753f7b6d21
Linmiao Xu <linmiao.xu@gmail.com>
2023-02-08 21:17:10 -0500

    Fix setting up 12-core workers

--- worker/run_workers.sh
+++ worker/run_workers.sh
@@ -7,16 +7,18 @@ for worker in $(seq 1 $num_16core_workers); do
   worker_dir=~/16core-worker$worker
   cp -r fishtest $worker_dir
   cd $worker_dir/worker
-  python3 worker.py $WORKER_CONFIG --concurrency 16 &
+  taskset -c $(( ($worker - 1) * 16 ))-$(( $worker * 16 - 1 )) \
+    python3 worker.py $WORKER_CONFIG --concurrency 16 &
   cd ~
 done
 
 num_12core_workers=$(( ( $(nproc) - 1 - ( $num_16core_workers * 16 ) ) / 12 ))
-for worker in $(seq 1 $num_8core_workers); do
-  worker_dir=~/8core-worker$worker
+for worker in $(seq 1 $num_12core_workers); do
+  worker_dir=~/12core-worker$worker
   cp -r fishtest $worker_dir
   cd $worker_dir/worker
-  python3 worker.py $WORKER_CONFIG --concurrency 12 &
+  taskset -c $(( $num_16core_workers * 16 + ($worker - 1) ))-$(( $num_16core_workers * 16 + $worker * 12 - 1 )) \
+    python3 worker.py $WORKER_CONFIG --concurrency 12 &
   cd ~
 done
 
