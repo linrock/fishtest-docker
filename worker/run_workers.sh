01f484d9e6e299414728f3631325189425e6c39e
Linmiao Xu <linmiao.xu@gmail.com>
2023-05-29 15:28:03 -0400

    Make all workers 16-core by default

--- worker/run_workers.sh
+++ worker/run_workers.sh
@@ -2,23 +2,13 @@
 
 git clone https://github.com/glinscott/fishtest ~/fishtest
 
-num_16core_workers=$(( ($(nproc) - 1 ) / 16 ))
+num_16core_workers=$(( ($(nproc)) / 16 ))
 for worker in $(seq 1 $num_16core_workers); do
   worker_dir=~/16core-worker$worker
   cp -r fishtest $worker_dir
   cd $worker_dir/worker
   taskset -c $(( ($worker - 1) * 16 ))-$(( $worker * 16 - 1 )) \
     python3 worker.py $WORKER_CONFIG --concurrency 16 &
-  cd ~
-done
-
-num_12core_workers=$(( ( $(nproc) - 1 - ( $num_16core_workers * 16 ) ) / 12 ))
-for worker in $(seq 1 $num_12core_workers); do
-  worker_dir=~/12core-worker$worker
-  cp -r fishtest $worker_dir
-  cd $worker_dir/worker
-  taskset -c $(( $num_16core_workers * 16 + ($worker - 1) ))-$(( $num_16core_workers * 16 + $worker * 12 - 1 )) \
-    python3 worker.py $WORKER_CONFIG --concurrency 12 &
   cd ~
 done
 
