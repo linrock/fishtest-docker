e4949cd9e7061aff457b28a136280d8391007aec
Linmiao Xu <linmiao.xu@gmail.com>
2023-08-01 21:18:06 -0400

    Single variable for concurrency

--- worker/run_workers.sh
+++ worker/run_workers.sh
@@ -2,13 +2,14 @@
 
 git clone https://github.com/glinscott/fishtest ~/fishtest
 
-num_16core_workers=$(( ($(nproc)) / 16 ))
-for worker in $(seq 1 $num_16core_workers); do
-  worker_dir=~/16core-worker$worker
+concurrency=32
+num_workers=$(( ($(nproc)) / $concurrency ))
+for worker in $(seq 1 $num_workers); do
+  worker_dir=~/32core-worker$worker
   cp -r fishtest $worker_dir
   cd $worker_dir/worker
-  taskset -c $(( ($worker - 1) * 16 ))-$(( $worker * 16 - 1 )) \
-    python3 worker.py $WORKER_CONFIG --concurrency 16 &
+  taskset -c $(( ($worker - 1) * $concurrency ))-$(( $worker * $concurrency - 1 )) \
+    python3 worker.py $WORKER_CONFIG --concurrency $concurrency &
   cd ~
 done
 
