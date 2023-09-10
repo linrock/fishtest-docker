9a313cc8210b8262cdd59340d6b3e06e36637fe3
Linmiao Xu <linmiao.xu@gmail.com>
2023-09-10 11:18:09 -0400

    Default to 16-core workers

--- worker/run_workers.sh
+++ worker/run_workers.sh
@@ -2,10 +2,10 @@
 
 git clone https://github.com/glinscott/fishtest ~/fishtest
 
-concurrency=32
+concurrency=16
 num_workers=$(( ($(nproc)) / $concurrency ))
 for worker in $(seq 1 $num_workers); do
-  worker_dir=~/32core-worker$worker
+  worker_dir=~/16core-worker$worker
   cp -r fishtest $worker_dir
   cd $worker_dir/worker
   taskset -c $(( ($worker - 1) * $concurrency ))-$(( $worker * $concurrency - 1 )) \
